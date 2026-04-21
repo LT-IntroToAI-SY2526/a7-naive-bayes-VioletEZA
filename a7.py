9333fe264eaa076b80197b2cacb7c57b12ce9a4b
VioletEZA <ezadan@cps.edu>
2026-04-21 10:53:05 -0500

    missing prints

--- a7.py
+++ a7.py
@@ -152,7 +152,8 @@ class BayesClassifier:
 
         # for debugging purposes, it may help to print the overall positive and negative
         # probabilities
-        
+        print(f"Positive Probability: {pos_score}")
+        print(f"Negative Probability: {neg_score}")
 
         # determine whether positive or negative was more probable (i.e. which one was
         # larger)
